09ce215974652424a4f0505b018f086ac8efc17f
a5chin <t319080@m.chukyo-u.ac.jp>
2022-01-22 04:36:01 +0900

    overwrite reshaper.py

--- lib/core/reshaper.py
+++ lib/core/reshaper.py
@@ -1,14 +1,32 @@
-from calendar import c
+import numpy as np
 import cv2
 
-gray = cv2.imread('../../data/problem/example2.png', cv2.IMREAD_GRAYSCALE)
-gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+gray = cv2.imread('data/problem/problem.png', cv2.IMREAD_GRAYSCALE)
 ret, th = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
+inv = cv2.bitwise_not(th)
 contours, hierarchy = cv2.findContours(
-    th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+    inv, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
 )
 
-img = cv2.drawContours(gray, contours, -1, (0,255,0), 3)
+max_area = 0
 
-cv2.imshow('sample', img)
+for cnt in contours:
+    arclen = cv2.arcLength(cnt, True)
+    approx_cnt = cv2.approxPolyDP(cnt, epsilon=0.001 * arclen, closed=True)
+    if len(approx_cnt) == 4:
+        area = cv2.contourArea(approx_cnt)
+        if area > max_area:
+            max_area = max(area, max_area)
+            contour = approx_cnt
+
+print(np.array(contour).reshape(4, -1))
+img = cv2.drawContours(inv, contour, -1, (255,255,255), 16)
+
+pts1 = np.array([[581, 79], [66, 192], [198, 733], [759, 571]], dtype=np.float32)
+pts2 = np.array([[800, 0], [0, 0], [0, 800], [800, 800]], dtype=np.float32)
+mat = cv2.getPerspectiveTransform(pts1, pts2)
+perspective_img = cv2.warpPerspective(img, mat, (800, 800))
+
+cv2.imshow('sample', perspective_img)
 cv2.waitKey(0)
